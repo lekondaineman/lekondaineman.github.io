63df8387ece7245aaffd73622ae18e01290f4d22
Leonid Dobrovolskiy <kabatovi1@gmail.com>
2019-09-28 17:56:46 +0300

    ромаха49

---
+++ js/main.js
@@ -0,0 +1,7 @@
+setTimeout (function(){
+	document.title = "НЕМАН СЛИЛИ!!!";
+}, 2000);
+
+$(function() {
+	$("b").delay(2000).fadeTo("slow", 1);
+	));
---
+++ romaxa49/js/main.js
@@ -0,0 +1,8 @@
+document.getElementById('romaxa').onclick=function() {
+    $('.bckground').animate({
+        opacity: 1,
+    }, 1500);
+    $('font').animate({
+        color: "#ffffff",
+    }, 1500);
+}
